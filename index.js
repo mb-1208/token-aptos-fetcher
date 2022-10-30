ca06ad37bdce4326c25a3871931e2b53b2351b88
mb-1208 <68389381+mb-1208@users.noreply.github.com>
2022-10-30 13:19:56 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const { AptosClient, TokenClient } = require("aptos");
+const basePath = process.cwd();
+const fs = require("fs");
+
+const client = new AptosClient('https://fullnode.mainnet.aptoslabs.com');
+const tokenClient = new TokenClient(client);
+
+const main = async () => {
+    let data = []
+    // console.log('asd');
+    let getData = await tokenClient.getCollectionData('0xc0e3fbf8ae61056d66ce624d71ccf1888f879355cc4e364ef117249b5e3160a8', 'Aptomingos');
+    console.log(getData);
+
+    for (let i = 0; i < 5; i++) {
+        const tokenData = await tokenClient.getTokenData('0xc0e3fbf8ae61056d66ce624d71ccf1888f879355cc4e364ef117249b5e3160a8', 'Aptomingos', 'Aptomingo' + ' #' + (i + 1).toString());
+        data.push(tokenData);
+    }
+    console.log(data);
+    fs.writeFileSync(`${basePath}/build/Aptomingos.json`, JSON.stringify(data, null, 2));
+};
+
+main();
