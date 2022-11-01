c547d2ef101b2b3fd957b7b5ba21c53b48dbd832
mb-1208 <68389381+mb-1208@users.noreply.github.com>
2022-11-02 05:56:55 +0700

    Update w-ipfsio.js

--- w-ipfsio.js
+++ w-ipfsio.js
@@ -11,7 +11,7 @@ const tokenClient = new TokenClient(client);
 const main = async () => {
     let data = []
     let getData = await tokenClient.getCollectionData('0xc0e3fbf8ae61056d66ce624d71ccf1888f879355cc4e364ef117249b5e3160a8', 'Aptomingos');
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < Number(getData.supply); i++) {
         const tokenData = await tokenClient.getTokenData('0xc0e3fbf8ae61056d66ce624d71ccf1888f879355cc4e364ef117249b5e3160a8', 'Aptomingos', 'Aptomingo' + " #" + (i + 1).toString());
         let tokenUri = tokenData.uri.replace("ipfs://", "https://ipfs.io/ipfs/");
         await axios.get(tokenUri).then((result) => {
